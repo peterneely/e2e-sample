95dd5e0b0d17102bb878302b1bd3acf1f259d532
unknown <Peter.Neely@CTDT-40FZ5Y1.corp.cts.ads>
2014-01-08 12:16:24 +0000

    More work on first scenario, first feature.

--- features/step_definitions/#XSELL01_Lightbox_steps.rb
+++ features/step_definitions/#XSELL01_Lightbox_steps.rb
@@ -1,9 +1,9 @@
 Given(/^an online customer is browsing the site on (.*)$/) do |locale|
-  on(HomePage).browse_to(locale).should be_true
+  on(HomePage).url_correct_for?(locale).should be_true
 end
 
 When(/^they add any one product to their shopping bag$/) do
-  #@current_page.add_product
+  on(HomePage).add_product
 end
 
 Then(/^they should see a minibag lightbox$/) do
